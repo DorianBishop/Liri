db43c5a732ae6883342eb5141a6f7d0448fc3af1
Dorian Bishop <dorianbishop427@gmail.com>
2020-01-02 12:23:38 -0500

    added spotify switch case

--- Liri.js
+++ Liri.js
@@ -1,2 +1,21 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+const input = process.argv.slice(3).join();
+var choice = process.argv[2];
+
+switch (choice) {
+    case "concert-this":
+        concert(input);
+        break;
+    case "spotify-this":
+        if (input) {
+            spotifyThis(input);
+        } else {
+            spotifyThis("Low Life");
+        }
+        break;
